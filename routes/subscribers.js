b472ea38ec99bf86c89f44a687e105f013864f40
James <jamesmca@live.com>
2021-09-21 10:04:16 -0700

    Update subscribers.js
    shell for all the basic routes to be created

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,4 +1,25 @@
 const express = require('express')
 const router = express.Router()
 
+// getting all
+router.get('/', (req, res) => {
+
+})
+//getting one
+router.get('/:id', (req, res) => {
+
+})
+//creating one
+router.post('/', (req, res) => {
+
+})
+//updating one (using patch inplace of put)
+router.patch('/:id', (req, res) => {
+
+})
+//deleting one
+router.delete('/:id', (req, res) => {
+
+})
+
 module.exports = router
